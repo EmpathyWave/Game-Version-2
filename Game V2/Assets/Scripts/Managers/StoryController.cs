c841820ffecb458d7a62aa974c5808edb10cd0fe
John <jw4429@nyu.edu>
2019-11-27 12:40:34 -0500

    Post Greenlight Push -Inkle test -canvas resizing fix -person not appearing fix -game state still a problem

--- Game V2/Assets/Scripts/Managers/StoryController.cs
+++ Game V2/Assets/Scripts/Managers/StoryController.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ink.Runtime;
 
 public class StoryController : MonoBehaviour
 {
+    private Story story;
+    public TextAsset inkJSONAsset;
+    public string currentKnot;
+    public string output;
+
+    void Awake()
+    {
+        story = new Story(inkJSONAsset.text);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,15 @@ public class StoryController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (story.canContinue)
+        {
+            output = story.Continue();
+        }
+    }
+
+    public void SetKnot(string character, string input) //set knot before you continue the story
+    {
+        currentKnot = character + "." + input;
+        story.ChoosePathString(currentKnot);
     }
 }
